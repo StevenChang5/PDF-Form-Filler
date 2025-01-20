e75aef871d57990145d2f9a0aaa08ff288a9f723
stevendee37 <stevenwchang01@gmail.com>
2025-01-20 09:57:32 -0800

    Implemented autofilling via csv file

---
+++ main.py
@@ -0,0 +1,18 @@
+import csv
+import os
+
+from pdf_forms import Form
+
+def main():
+    with open('tenants.csv',mode='r') as file:
+        csvFile = csv.reader(file)
+        headers = next(csvFile)
+        for lines in csvFile:
+            tenant_1 = lines[0]
+            tenant_2 = lines[1]
+            rent = int(lines[2])
+            form = Form(tenant_1,tenant_2,rent)
+            form.all()
+
+if __name__ == "__main__":
+    main()
